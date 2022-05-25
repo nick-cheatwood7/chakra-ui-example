2e3a54a8e374ff4ce1aa0bc29cd35ba3a993b1e3
nick-cheatwood7 <nick.cheatwood@gmail.com>
2022-05-25 17:25:48 -0500

    finishing touches on initial page

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,30 @@ import {
   Button,
   Container,
   Input,
-  Stack,
-  Switch,
   useColorMode,
   VStack,
-  Text,
   Heading,
 } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 const App: React.FC = () => {
-  const { toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Container maxW="md" height="100vh">
       <Button
+        variant="ghost"
         pos="absolute"
         inset="0 0 auto auto"
         m="4"
         onClick={toggleColorMode}
       >
-        ?
+        {colorMode === "light" ? <MoonIcon color="gray" /> : <SunIcon />}
       </Button>
-      <VStack justify="center" height="100%" spacing="4">
+      <VStack justify="center" height="100%" spacing="5">
         <Heading textAlign="center">React + Chakra UI = ❤️</Heading>
         <Input placeholder="username" />
         <Input placeholder="password" type="password" />
-        <Button colorScheme="teal" variant="outline" width="100%">
+        <Button colorScheme="teal" variant="outline" width="75%">
           Log In
         </Button>
         <Button colorScheme="gray" variant="link">
